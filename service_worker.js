dece5d4c0e10a40dd782c662a3e07ca212d44852
ArnieKeys <boesack@gmail.com>
2025-07-15 17:08:05 +0800

    Add offline support for PWA

--- service_worker.js
+++ service_worker.js
@@ -1,45 +1,26 @@
 const CACHE_NAME = "text-reader-cache-v1";
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png",
+  "./",
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "assets/icons/icon-192.png",
+  "assets/icons/icon-512.png",
 ];
 
-// Install Service Worker and cache files
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Caching app shell");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
-// Serve files from cache first
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
-  );
-});
-
-// Update cache if needed
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log("Removing old cache", key);
-            return caches.delete(key);
-          }
-        })
-      )
-    )
   );
 });
